0071f71f51e65c87135be33d72c8e2aabb469467
hozenyth.andrade <hozenyth.andrade@gmail.com>
2024-11-01 09:48:06 -0300

    atualização

--- obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7b277290dec73437a35fcab32827b0ecb4292f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f70119e3faf76061e346e699edbcb6646cda74ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
